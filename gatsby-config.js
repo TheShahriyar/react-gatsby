468633b4d5b6090695ec3458e73ebaeda6a5f956
Shahriyar Ahmed <shahriyar.31dec@gmail.com>
2020-01-04 17:35:45 +0600

    Homepage updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,29 @@
 
 module.exports = {
   /* Your site config here */
+
+  siteMetadata: {
+    title: `Othelo - Onepage template`,
+    description: "This is template description and the text will be long.",
+    author: "TheShahriyar",
+  },
+
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `fonts`,
+        path: `${__dirname}/src/fonts/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Kaushan Script", "Poppins"],
+        },
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../css/animate.css"
+import "../css/lineicon.css"
+import "../css/font-awesome.min.css"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import "../scss/style.scss"
+import "../scss/responsive.scss"
+import HomepageBanner from "./homepageBanner"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <>
+      <HomepageBanner />
+    </>
+  )
+}
